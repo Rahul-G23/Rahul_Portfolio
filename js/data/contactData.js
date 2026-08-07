d42c5ef99ae325ed63385e549dee721f953752c3
Rahul-G23 <Rahul-G23@users.noreply.github.com>
2026-08-07 12:09:14 +0530

    Add contact information data

---
+++ js/data/contactData.js
@@ -0,0 +1,64 @@
+const CONTACT = {
+
+    tag: "Contact",
+
+    title: "Let's Connect",
+
+    subtitle:
+        "Interested in collaborating, discussing internship opportunities, hackathons, or software development projects? Feel free to reach out through any of the platforms below.",
+
+    info: [
+
+        {
+            icon: "fa-solid fa-envelope",
+
+            title: "Email",
+
+            value: "rahulg23102006@gmail.com",
+
+            link: "mailto:rahulg23102006@gmail.com"
+        },
+
+        {
+            icon: "fa-solid fa-phone",
+
+            title: "Phone",
+
+            value: "+91 63611 46881",
+
+            link: "tel:+916361146881"
+        },
+
+        {
+            icon: "fa-solid fa-location-dot",
+
+            title: "Location",
+
+            value: "Bengaluru, Karnataka, India",
+
+            link: "https://maps.google.com/?q=Bengaluru,Karnataka,India"
+        },
+
+        {
+            icon: "fa-brands fa-linkedin",
+
+            title: "LinkedIn",
+
+            value: "linkedin.com/in/rahul-g23",
+
+            link: "https://www.linkedin.com/in/rahul-g23"
+        },
+
+        {
+            icon: "fa-brands fa-github",
+
+            title: "GitHub",
+
+            value: "github.com/Rahul-G23",
+
+            link: "https://github.com/Rahul-G23"
+        }
+
+    ]
+
+};
